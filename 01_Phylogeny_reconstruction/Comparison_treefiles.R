9efe6ad77fd15f5965b15f389d33767daa367434
Yuxuan Liu <146070633+Yuxuanliu-HZAU@users.noreply.github.com>
2026-09-15 22:01:11 +0800

    Update Comparison_treefiles.R

--- 01_Phylogeny_reconstruction/Comparison_treefiles.R
+++ 01_Phylogeny_reconstruction/Comparison_treefiles.R
@@ -15,22 +15,26 @@ get_tip_colors <- function(tree, species_list = yellow_species, highlight_color 
   return(tip_colors)
 }
 
-# ---------------- 参数（按需修改） ----------------
+# ---------------- Parameters ----------------
 comparison_mode <- "group"   # "single" or "group"
-work_dir <- "D:/Projects/Spiraea/03-Phylogenetic_trees/100/ASTRAL-IV-China_final2/"
-ref_tree <- "D:/Projects/Spiraea/03-Phylogenetic_trees/100/ASTRAL-IV-China_final2/ASTRAL4_HybSuite_HRS.bootstrap.rr.tre"
-right_tree <- "D:/Projects/Spiraea/03-Phylogenetic_trees/Summary_tree/Final_species_tree_ASTRAL-IV.tre"
+work_dir <- "./"
+ref_tree <- "./01_Phylogeny_reconstruction/ASTRAL4_HybSuite_HRS.bootstrap.rr.tre"
+# right_tree <- "./01_Phylogeny_reconstruction/Final_species_tree_ASTRAL-IV.tre"
 output_file <- "Final_ASTRAL-IV_comparison"
 output_type <- "pdf"         # "png" or "pdf"
 line_alpha <- 0.9           # numeric 0..1
 single_title <- "Summary tree: MI+MO vs original 1to1"
-outgroup_tips <- c("Sibiraea_angustata", "Petrophytum_caespitosum", "Holodiscus_discolor", "Luetkea_pectinata", "Pentactina_rupicola", "Xerospiraea_hartwegiana", "Aruncus_dioicus")  # vector or NULL
-# 100: c("Sibiraea_angustata", "Petrophytum_caespitosum") 
-# 353: "Sibiraea_angustata"
+outgroup_tips <- c("Sibiraea_angustata", "Petrophytum_caespitosum")  # vector or NULL
+# Arabidopsis100: c("Sibiraea_angustata", "Petrophytum_caespitosum") 
+# Angiosperms353: "Sibiraea_angustata"
 cophylo_line_color <- "clade" # "single" or "clade"
 show_branch_length <- FALSE  # FALSE => remove edge.length (topology only)
 remove_outgroups <- FALSE    # logical: if true, will remove outgroup_tips in process
-clade_table <- "Clade.tsv"
+# Clades.tsv can be downloaded here:
+# Angiosperms353:
+# Arabidopsis100: 
+clade_table <- "Clade.tsv" 
+
 color_branches <- TRUE     # logical: if true, color tree branches according to clade.tsv colors
 # --------------------------------------------------
 
